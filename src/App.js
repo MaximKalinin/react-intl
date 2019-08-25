f3feaf729d4464cc273868e7302f912d92563314
Maksim <maxim.kalinin.work@gmail.com>
2019-08-25 15:15:47 +0300

    next commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
+import { FormattedMessage, IntlProvider } from 'react-intl';
+
 import logo from './logo.svg';
 import './App.css';
+import messages_de from './translations/de.json';
+import messages_en from './translations/en.json';
+import messages_ru from './translations/ru.json';
 
-function App() {
+const messages = {
+  'de': messages_de,
+  'en': messages_en,
+  'ru': messages_ru
+};
+
+const App = props => {
+  const [locale, setLocale] = React.useState(navigator.language.split(/[-_]/)[0]);
+  const chooseLocale = event => {
+    setLocale(event.target.id || 'en');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={ locale } messages={ messages[locale] }>
+      <div className="App">
+        <header className="App-header">
+          <ul className="language-choose">
+            <li onClick={ chooseLocale } id="ru">Русский</li>
+            <li onClick={ chooseLocale } id="en">English</li>
+            <li onClick={ chooseLocale } id="de">Deutsch</li>
+          </ul>
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            <FormattedMessage id="main.to-get-started" />
+            <code> src/App.js </code>
+            <FormattedMessage id="main.save-to-reload" />
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FormattedMessage id="main.learn-react-link" defaultMessage="Learn React" />
+          </a>
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
